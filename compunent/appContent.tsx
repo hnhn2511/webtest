312a56e1c84e0193604f0e925871816b7a1fe736
Hnhn2511 <Hnhn2511@gmail.com>
2023-11-27 16:22:07 +0700

    init

--- compunent/appContent.tsx
+++ compunent/appContent.tsx
@@ -1,29 +1,31 @@
 
 import facebook from '../styles/facebook.module.css';
 import Link from 'next/link';
-import ReactPaginate from 'react-paginate';
 import { useState } from 'react';
-// import '../styles/paginate.css';
+import { Pagination } from '@mui/material';
 
 function Contenthome(props: IDatacop) {
 
     const { items } = props;
 
-    const itemsPerPage = 15;
-    const [currentPage, setCurrentPage] = useState<number>(0);
+    const itemsPerPage = 1;
+    const totalPages = Math.ceil(items.length / itemsPerPage);
 
-    const handlePageChange = (selectedPage: { selected: number }) => {
-        setCurrentPage(selectedPage.selected);
+    const paginatedData = Array.from({ length: totalPages }, (_, index) =>
+        items.slice(index * itemsPerPage, (index + 1) * itemsPerPage)
+    );
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleChange = (event: React.ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
     };
 
-
-    const indexOfLastItem = (currentPage + 1) * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+    const currentPageData = paginatedData[currentPage - 1] || [];
 
     return (
         <>
-            {currentItems.map(item => {
+            {currentPageData.map(item => {
                 return (
                     <div key={item.id} className={facebook.columm10columm}>
                         <div className={facebook.columm10colummContent}>
@@ -44,26 +46,15 @@ function Contenthome(props: IDatacop) {
                 )
             })}
 
-            <ReactPaginate
-                breakLabel="..."
-                nextLabel="Next >"
-                onPageChange={handlePageChange}
-                pageRangeDisplayed={2}
-                marginPagesDisplayed={2}
-                pageCount={Math.ceil(items.length / itemsPerPage)}
-                previousLabel="< Previous"
-                renderOnZeroPageCount={null}
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
-                containerClassName="pagination"
-                activeClassName="active"
+            <Pagination
+                count={totalPages}
+                page={currentPage}
+                onChange={handleChange}
+                variant="outlined"
+                shape="rounded"
+                style={{ width: '100%', justifyContent: 'center', margin: '20px 0', display: 'flex' }}
             />
+
         </>
     )
 }
