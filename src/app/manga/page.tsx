f26896e26e9ed0bc802e25d63e2b21aaae9e4c34
Hnhn2511 <Hnhn2511@gmail.com>
2023-12-04 21:12:23 +0700

    init

---
+++ src/app/manga/page.tsx
@@ -0,0 +1,18 @@
+
+import Container from '../../../compunent/appContainer';
+import type { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+    title: 'Manga',
+    description: 'Trang Manga của Lib Coop ',
+  }
+
+function Manga() {
+
+
+    return (
+        <Container></Container>
+    )
+}
+export default Manga;
